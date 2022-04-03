e43161326ef762cb8ac14e65f8774dbd2bd38cb0
go <hingadesuraj7620@gmail.com>
2022-04-04 00:36:02 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Home from './components/Home';
-import { render } from "react-dom";
+ 
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 import UserInfo from './components/UserInfo';
-// import StartGame from './components/StartGame';
+ 
 import Buttons from './components/Buttons';
-// import Startgame from '../components/StartGame';
+ 
 import Startgame from '../src/components/Startgame';
  
 
@@ -21,9 +21,9 @@ function App() {
 <Routes>
 <Route path='/' element={<Home />} />
 <Route path='/user' element={<UserInfo />} />
-{/* <Route path='/start' element={<StartGame />} /> */}
+ 
 <Route path='/button' element={<Buttons />} />
-{/* <Route path='/suraj' element={</>} /> */}
+ 
 <Route path='/startgame' element={<Startgame />} />
 </Routes>  
 </BrowserRouter>
